cafb4cf649f0499fdaf3a703985262f8ff43106a
Gamesscow <jack.crampton@yahoo.com>
2023-01-27 23:24:52 +0000

    Task 6 Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //check for divisibility by 4
+    if (year % 4 == 0 && year % 100 != 0){
+        return true;
+
+    }
+    if (year % 100 == 0 && year % 400 == 0){
+        return true;
+    }
+    return false;
 
 };
 
